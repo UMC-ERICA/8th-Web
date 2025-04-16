b4360be53d2c2b05d9833c16700bf2ac3a4818f2
atank3566 <atank3566@naver.com>
2025-04-16 15:28:53 +0900

    4주차 미션_후이

--- 후이-박희웅/LP/LP_Project/src/constants/key.ts
+++ 후이-박희웅/LP/LP_Project/src/constants/key.ts
@@ -1,12 +1,4 @@
-import axios from "axios";
-
 export const LOCAL_STORAGE_KEY = {
   ACCESS_TOKEN: "accessToken",
 } as const;
 
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN)}`,
-  },
-});
