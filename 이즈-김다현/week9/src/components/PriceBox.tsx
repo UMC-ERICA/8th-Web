9cba3e351fa3b528e902f466479ba48bb6c8454a
DADAIZGOOD <dachord@hanyang.ac.kr>
2025-05-26 09:16:44 +0900

    total 오류 수정

--- 이즈-김다현/week9/src/components/PriceBox.tsx
+++ 이즈-김다현/week9/src/components/PriceBox.tsx
@@ -1,20 +1,25 @@
-import { useDispatch, useSelector } from "../hooks/useCustomRedux"
-import { clearCart, type CartState } from "../slices/cartSlice";
+import { useCartActions, useCartInfo } from "../hooks/useCartStore";
+import { useEffect } from "react";
 
 const PriceBox = () => {
-    const {total} = useSelector((state) : CartState => state.cart);
-    const dispatch = useDispatch();
+    const {total} = useCartInfo();
+    const {clearCart, calculateTotals} = useCartActions();
+
+    useEffect(() => {
+        calculateTotals();
+    }, [calculateTotals]);
 
     const handleInitalizeCart = () => {
-        dispatch(clearCart());
+        clearCart();
     }
 
-  return (
-    <div className="p-12 flex justify-between">
-        <button onClick={handleInitalizeCart} className="border p-2 py-4 rounded-md cursor-pointer"> 장바구니 초기화</button>
-        총 가격 : {total}원
-    </div>
-  )
+    return (
+        <div className="p-12 flex justify-between">
+            <button onClick={handleInitalizeCart} className="border p-2 py-4 rounded-md cursor-pointer"> 장바구니 초기화</button>
+            총 가격 : {total}원
+        </div>
+    )
 }
 
 export default PriceBox
+
